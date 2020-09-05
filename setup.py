ff1d15bd55bace336bbcc5ee555272d1f81733e8
William Di Cicco <nearata@protonmail.com>
2020-09-05 16:55:12 +0200

    v0.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="hello-world-nearata",
-    version="0.0.4",
+    version="0.0.5",
     author="Nearata",
     author_email="williamdicicco@protonmail.com",
     description="Hello World!",
